e918c9c479e45f08bf20e4dfba1673fadc3becfe
MB <mb@blaster.ai>
2022-06-30 14:08:52 +0200

    Add package command

--- build.clj
+++ build.clj
@@ -1,9 +1,12 @@
 (ns build
   (:refer-clojure :exclude [test])
-  (:require [org.corfield.build :as bb]))
+  (:require [org.corfield.build :as bb]
+            [clojure.java.shell :refer [sh]]
+            [clojure.java.io :as io]))
 
+(def package-name "Sanskrit Dictionary by MB")
 (def lib 'net.clojars.licht1stein/sanskrit-desktop-dict)
-(def version "0.1.0-SNAPSHOT")
+(def version "0.1.0")
 (def main 'sanskrit-desktop-dict.core)
 
 (defn test "Run the tests." [opts]
@@ -15,3 +18,13 @@
       #_(bb/run-tests)
       (bb/clean)
       (bb/uber)))
+
+(defn package "Package the application" [opts]
+  (let [uberjar (str "sanskrit-desktop-dict-" version ".jar")]
+    (when-not (.exists (io/file (str "target/" uberjar)))
+      (println  (str "Error: uberjar target/" uberjar " not found. Did you forget to run the ci command?\n\nRun before packaging:\nclj -T:build ci\n"))
+      (System/exit 1))
+    (println (str "Packaging target/" uberjar))
+    (-> (sh  "jpackage" "--name" package-name "--input" "target" "--main-jar" uberjar)
+        :out
+        println)))
