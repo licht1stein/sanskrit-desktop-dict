40510317f9aa1e42571d20d21106ea606d2117e5
MB <mb@blaster.ai>
2022-06-30 14:57:06 +0200

    Add build commands to package mac app

--- build.clj
+++ build.clj
@@ -6,7 +6,7 @@
 
 (def package-name "Sanskrit Dictionary by MB")
 (def lib 'net.clojars.licht1stein/sanskrit-desktop-dict)
-(def version "0.1.0")
+(def version "1.0.0")
 (def main 'sanskrit-desktop-dict.core)
 
 (defn test "Run the tests." [opts]
@@ -19,12 +19,35 @@
       (bb/clean)
       (bb/uber)))
 
-(defn package "Package the application" [opts]
-  (let [uberjar (str "sanskrit-desktop-dict-" version ".jar")]
-    (when-not (.exists (io/file (str "target/" uberjar)))
+(defn replace-icon [opts]
+  (let [icon-file (io/file "resources/app-icon.icns")]
+    (io/copy icon-file (io/file (str package-name ".app/Contents/Resources/" package-name ".icns")))))
+
+(defn package-image [opts]
+  (println (sh  "jpackage"
+                "--name" package-name
+                "--app-image" (str  "./" package-name ".app"))))
+
+(defn package "Package the application with jpackage" [opts]
+  (let [uberjar (str "sanskrit-desktop-dict-" version ".jar")
+        uberjar-exists? (.exists (io/file (str "target/" uberjar)))
+        icon-file (io/file "resources/app-icon.icns")
+        icon-exists? (.exists icon-file)]
+    (when-not uberjar-exists?
       (println  (str "Error: uberjar target/" uberjar " not found. Did you forget to run the ci command?\n\nRun before packaging:\nclj -T:build ci\n"))
       (System/exit 1))
-    (println (str "Packaging target/" uberjar))
-    (-> (sh  "jpackage" "--name" package-name "--input" "target" "--main-jar" uberjar)
-        :out
-        println)))
+    (println (str "Packaging target/" uberjar " as image"))
+    (let [result (-> (sh  "jpackage"
+                          "--name" package-name
+                          "--input" "target"
+                          "--main-jar" uberjar
+                          "--app-version" version
+                          "--copyright" "Mikhail Beliansky"
+                          "--type" "app-image"))]
+      (println result)
+      (println "Replacing icon...")
+      (replace-icon nil)
+      (println "Packaging image...")
+      (package-image nil)
+      (println "Cleaning up...")
+      (println (sh "rm" "-rf" (str package ".app"))))))
