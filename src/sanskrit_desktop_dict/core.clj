b32eb33626b1083a287f1966c2d6be2ccd07342d
MB <mb@blaster.ai>
2022-07-01 12:58:12 +0200

    UX first version ready

--- src/sanskrit_desktop_dict/core.clj
+++ src/sanskrit_desktop_dict/core.clj
@@ -5,26 +5,113 @@
             [cljfx.lifecycle :as fx.lifecycle]
             [cljfx.css :as css]
             [clojure.string :as str]
-            [hiccup.core :refer [html]])
+            [clojure.edn :as edn]
+            [clojure.math :as math]
+            [clojure.java.io :as io]
+            [clojure.core.cache :as cache]
+            [clojure.pprint :refer [pprint]]
+            [hiccup.core :refer [html]]
+            [taoensso.timbre :as timbre])
   (:import [javafx.scene.input KeyCode KeyEvent]
            [javafx.scene.web WebView])
   (:gen-class))
 
 (def sample "Benfey Sanskrit-English Dictionary - 1866 नर नर, i. e. नृ + अ, m. 1. A man; pl. Men, Man. 1, 96. 2. The Eternal, the divine imperishable spirit pervading the universe, Man. 1, 10. 3. pl. Cer tain fabulous beings, MBh. 2, 396. 4. A proper name, Bhāg. P. 8, 1, 27. — Cf. Lat. Nero, Neriene.")
 
-(def *state (atom {:settings {:zoom 100
-                              :history {:max-size 20}}
-                   :title "Sanskrit Dictionaries"
-                   :status "Ready!"
-                   :input {:current {:original ""
-                                     :translation ""}
-                           :history []}}))
+(def default-settings {:zoom 100
+                       :history {:max-size 20}})
 
+(defn save-settings [settings]
+  (let [settings-dir (str (System/getProperty "user.home") "/.sanskrit-dict")
+        settings-file (str settings-dir "/settings.edn")]
+    (spit settings-file settings)))
+
+(defn load-settings []
+  (timbre/info ::load-settings :start)
+  (let [settings-dir (str (System/getProperty "user.home") "/.sanskrit-dict")
+        settings-file (str settings-dir "/settings.edn")]
+    (when-not (.exists (io/file settings-dir))
+      (timbre/warn ::load-settings "Creating settings directory")
+      (.mkdir (io/file settings-dir)))
+    (when-not (.exists (io/file settings-file))
+      (timbre/warn ::load-settings "Creating settings file with defaults")
+      (spit settings-file default-settings))
+    (-> settings-file slurp edn/read-string)))
+
+
+(def *state
+  (atom
+   (fx/create-context
+    {:settings (load-settings)
+     :title "Sanskrit Dictionaries"
+     :status "Ready!"
+     :input {:current {:original ""
+                       :transliteration ""
+                       :translation ""
+                       :dictionaries []}
+             :history []}}
+    cache/lru-cache-factory)))
+
+;; Subscription functions
+(defn sub:title [context]
+  (fx/sub-val context get-in [:title]))
+
+
+;; Event handlers with helpers
 (defn history-conj [{:keys [settings input] :as state}  value]
   (let [new-history (-> (take (-  (-> settings :history :max-size) 1) (:history input))
                         (conj value))]
     (assoc-in state [:input :history] new-history)))
 
+(defn new-search! [state word]
+  (swap! state  fx/swap-context #(-> %
+                                     (history-conj word)
+                                     (assoc-in [:input :current :original] word))))
+
+(defmulti event-handler :event/type)
+
+(defmethod event-handler ::word-selected [event & args]
+  (new-search! *state (:fx/event event)))
+
+(defmethod event-handler ::action [event & args]
+  (let [value (-> event :fx/event .getTarget .getValue)]
+    (new-search! *state value)))
+
+(defmethod event-handler ::save-settings [{:keys [fx/context]}]
+  (let [settings (-> context :cljfx.context/m :settings)]
+    (timbre/info ::event-handler :save-settings)
+    (save-settings settings)
+    {:dispatch {:event/type ::temp-status
+                :value "Settings saved!"
+                :timeout 1}}))
+
+(defmethod event-handler ::temp-status [event]
+  (timbre/info ::temp-status event)
+  (swap! *state fx/swap-context assoc-in [:status] (:value event))
+  (future
+    (Thread/sleep (* 1000 (or (:timeout event) 2)))
+    (swap! *state fx/swap-context assoc-in [:status] "Ready!")))
+
+
+(defmethod event-handler ::zoom-change [{:keys [fx/event] :as data}]
+  (swap! *state fx/swap-context assoc-in [:settings :zoom] event)
+  {:dispatch {:event/type ::save-settings}})
+
+
+(def final-event-handler
+  (-> event-handler
+      (fx/wrap-co-effects
+       {:fx/context (fx/make-deref-co-effect *state)})
+      (fx/wrap-effects
+       {:context (fx/make-reset-effect *state)
+        :dispatch fx/dispatch-effect
+        :log #(println "DEBUG |" (str %))})))
+
+(comment
+  (final-event-handler {:event/type ::zoom-change
+                        :fx/event 166}))
+
+;; Views
 
 (defn component:word-input-combo [value items]
   {:fx/type :combo-box
@@ -40,7 +127,7 @@
    ;; :on-key-pressed {:event/type ::press}
    :on-action {:event/type ::action}})
 
-(defn component:dictionaries-combo [state & {:keys [row column]}]
+(defn component:dictionaries-combo [& {:keys [row column]}]
   {:fx/type :combo-box
    :max-width 600
    :pref-width 500
@@ -75,31 +162,20 @@
    :items [{:fx/type :button
             :text "btn"}]})
 
-(defn perc->long [s]
-  (-> s
-      (str/replace #"%" "")
-      parse-long))
-
-(defn long->perc [i]
-  (str i "%"))
-
-(comment
-  (perc->long "110%")
-  (long->perc 100))
-
-
-(defn stage:main [{:keys [input title status settings] :as state}]
-  (let [input? (not= "" (-> input :current :original))
-        zoom (-> settings :zoom)
-        style (css/register ::style
-                            {".root" {:-fx-font-size (str (* zoom 0.01) "em")}})]
+(defn stage:main [{:keys [fx/context]}]
+  (let [title (fx/sub-ctx context sub:title) ;; sample use of sub function
+        input (fx/sub context :input)
+        settings (fx/sub context :settings)
+        input? (not= "" (-> input :current :original))
+        zoom (:zoom settings)
+        status (fx/sub context :status)
+        style (css/register ::style {".root" {:-fx-font-size (str (* zoom 0.01) "em")}})]
     {:fx/type :stage
      :showing true
      :title title
      :scene {:fx/type :scene
              :stylesheets [(::css/url style)]
              :root {:fx/type :grid-pane
-                    :grid-lines-visible true
                     :padding 1
                     :column-constraints [{:fx/type :column-constraints
                                           :percent-width 100}]
@@ -118,14 +194,18 @@
                                                     {:fx/type :separator}
                                                     {:fx/type :label
                                                      :text "Dictionaries"}
-                                                    (component:dictionaries-combo state :row 0 :column 1)
+                                                    (component:dictionaries-combo :row 0 :column 1)
                                                     {:fx/type :separator}
                                                     {:fx/type :label
-                                                     :text "Font"}
-                                                    {:fx/type :combo-box
-                                                     :value (-> settings :zoom long->perc)
-                                                     :items (map long->perc [90 95 100 110 125 150 175 200])
-                                                     :on-action {:event/type ::zoom}}]}]}
+                                                     :text "Zoom"}
+                                                    {:fx/type :slider
+                                                     :value zoom
+                                                     :min 100
+                                                     :max 200
+                                                     :major-tick-unit 25
+                                                     :block-increment 5
+                                                     :show-tick-marks true
+                                                     :on-value-changed {:event/type ::zoom-change}}]}]}
 
                                ;; Middle row: word selector and translation
                                {:fx/type :grid-pane
@@ -142,44 +222,28 @@
                                                                 :column 0
                                                                 :row 0)
                                            ;; translation
-                                           (component:translation (if input? sample "Type and press enter to start searching...") :column 1 :row 0)]}
+                                           (component:translation (if input?
+                                                                    (str (-> input :current :original) "<br><br>" sample)
+                                                                    "Type and press enter to start searching...") :column 1 :row 0)]}
 
                                {:fx/type :label
-                                :text (str "History: " (-> input :history str))
+                                :text status
                                 :grid-pane/column 0
                                 :grid-pane/row 2}]}}}))
-
-(defn new-search! [state word]
-  (swap! state #(-> %
-                    (history-conj word)
-                    (assoc-in [:input :current :original] word))))
-
-(defmulti event-handler :event/type)
-
-(defmethod event-handler ::type [event]
-  (swap! *state assoc-in [:input :current] (:fx/event event)))
-
-(defmethod event-handler ::press [event]
-  (when (= KeyCode/ENTER (.getCode ^KeyEvent (:fx/event event)))
-    (swap! *state #(-> %
-                       (update-in [:input :history] conj (-> % :input :current))))))
-
-(defmethod event-handler ::word-selected [event]
-  (new-search! *state (:fx/event event)))
-
-(defmethod event-handler ::action [event]
-  (let [value (-> event :fx/event .getTarget .getValue)]
-    (new-search! *state value)))
-
-(defmethod event-handler ::zoom [event]
-  (let [value (-> event :fx/event .getTarget .getValue)]
-    (swap! *state assoc-in [:settings :zoom] (perc->long value))))
 
 
 (def renderer
   (fx/create-renderer
-   :middleware (fx/wrap-map-desc assoc :fx/type stage:main)
-   :opts {:fx.opt/map-event-handler event-handler}))
+   :middleware (comp
+                ;; Pass context to every lifecycle as part of option map
+                fx/wrap-context-desc
+                (fx/wrap-map-desc (fn [_] {:fx/type stage:main})))
+   :opts {:fx.opt/map-event-handler final-event-handler
+          :fx.opt/type->lifecycle #(or (fx/keyword->lifecycle %)
+                                       ;; For functions in `:fx/type` values, pass
+                                       ;; context from option map to these functions
+                                       (fx/fn->lifecycle-with-context %))}))
+
 
 (defn -main [& args]
   (fx/mount-renderer
@@ -187,3 +251,6 @@
    renderer))
 
 (-main)
+
+(comment
+  (renderer))
