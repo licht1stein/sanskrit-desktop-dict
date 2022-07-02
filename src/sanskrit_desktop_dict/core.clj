ee867785ff3ab1790b35d528f19d7d2456935df8
MB <mb@blaster.ai>
2022-07-02 06:22:08 +0200

    Dict selector as a tab

--- src/sanskrit_desktop_dict/core.clj
+++ src/sanskrit_desktop_dict/core.clj
@@ -9,14 +9,14 @@
             [clojure.core.cache :as cache]
             [hiccup.core :refer [html]]
             [taoensso.timbre :as timbre]
+            [sanskrit-desktop-dict.db.core :as db]
             [sanskrit-desktop-dict.helpers :as helpers])
   (:import [javafx.scene.web WebView])
   (:gen-class))
 
 (def sample "Benfey Sanskrit-English Dictionary - 1866 नर नर, i. e. नृ + अ, m. 1. A man; pl. Men, Man. 1, 96. 2. The Eternal, the divine imperishable spirit pervading the universe, Man. 1, 10. 3. pl. Cer tain fabulous beings, MBh. 2, 396. 4. A proper name, Bhāg. P. 8, 1, 27. — Cf. Lat. Nero, Neriene.")
-
-(def default-settings {:zoom 100
-                       :history {:max-size 20}})
+`(def default-settings {:zoom 100
+                        :history {:max-size 20}})
 
 (defn save-settings [settings]
   (let [settings-dir (str (System/getProperty "user.home") "/.sanskrit-dict")
@@ -40,8 +40,10 @@
   (atom
    (fx/create-context
     {:settings (load-settings)
+     :current-view {:translation :translation}
      :title "Sanskrit Dictionaries"
      :status "Ready!"
+     :dictionaries (db/all-dictionaries db/ds)
      :input {:current {:original ""
                        :transliteration ""
                        :translation ""
@@ -66,6 +68,14 @@
       (assoc-in [:input :current :original] word)))
 
 (defmulti event-handler :event/type)
+
+(defmethod event-handler ::toggle-dictionaries [{:keys [fx/event fx/context]}]
+  (timbre/debug ::toggle-dictionaries)
+  (let [mode (-> context :cljfx.context/m :current-view :translation)
+        _ (timbre/debug ::toggle-dictionaries {:old-mode mode})
+        new-mode (if (= mode :translation) :dict-selector :translation)]
+    (timbre/debug ::toggle-dictionaries {:new-mode new-mode})
+    {:context (fx/swap-context context assoc-in [:current-view :translation] new-mode)}))
 
 (defmethod event-handler ::new-search [{:keys [value fx/context] :as data}]
   {:context (fx/swap-context context new-search! value)})
@@ -99,6 +109,8 @@
   {:context (fx/swap-context context assoc-in [:settings :zoom] (helpers/perc->long event))
    :dispatch {:event/type ::save-settings}})
 
+(defmethod event-handler ::dicionary-selected [{:keys [fx/event fx/context]}]
+  (timbre/debug ::dicionary-selected event))
 
 (def final-event-handler
   (-> event-handler
@@ -129,14 +141,6 @@
    ;; :on-key-pressed {:event/type ::press}
    :on-action {:event/type ::action}})
 
-(defn component:dictionaries-combo [& {:keys [row column]}]
-  {:fx/type :combo-box
-   :max-width 600
-   :pref-width 500
-   ;; :grid-pane/column column
-   ;; :grid-pane/row row
-   :items ["afdaf dafasav" "de"]})
-
 (def ext-with-html
   (fx/make-ext-with-props
    {:html (fx.prop/make
@@ -144,11 +148,11 @@
            fx.lifecycle/scalar)}))
 
 (defn component:translation [html & {:keys [row column]}]
-  {:fx/type ext-with-html
-   :props {:html html}
-   :grid-pane/column column
-   :grid-pane/row row
-   :desc {:fx/type :web-view}})
+  (cond-> {:fx/type ext-with-html
+           :props {:html html}
+           :desc {:fx/type :web-view}}
+    (some? column) (assoc :grid-pane/column column)
+    (some? row) (assoc :grid-pane/row row)))
 
 (defn component:word-list [items & {:keys [row column]}]
   {:fx/type :list-view
@@ -156,13 +160,6 @@
    :grid-pane/column column
    :grid-pane/row row
    :on-selected-item-changed {:event/type ::word-selected}})
-
-(defn component:dictionaries-toolbar [items]
-  {:fx/type :tool-bar
-   :grid-pane/column 1
-   :grid-pane/row 0
-   :items [{:fx/type :button
-            :text "btn"}]})
 
 (defn component:zoom-combo [value]
   {:fx/type :combo-box
@@ -177,14 +174,82 @@
     (some? column) (assoc :grid-pane/column column)
     (some? row) (assoc :grid-pane/row row)))
 
+(defn components:dictionaries-checkboxes [dicts & {:keys [row column]}]
+  (cond-> {:fx/type :v-box
+           :children (into [{:fx/type :label
+                             :text "Dictionaries"}] (for [d dicts] {:fx/type :check-box
+                                                                    :text (:name d)}))}
+    (some? column) (assoc :grid-pane/column column)
+    (some? row) (assoc :grid-pane/row 0)))
+
+(defn component:dictionaries-tree [dicts]
+  (let [items (for [d dicts]
+                {:fx/type :tree-cell})]
+    {:fx/type :tree-view
+     :selection-mode :multiple
+     :grid-pane/row 0
+     :grid-pane/column 1
+     :root {:fx/type :tree-item
+            :value "All Dictionaries"
+            :expanded true
+            :children [{:fx/type :tree-item
+                        :expanded true
+                        :value "Sanskrit -> English"
+                        :children [{:fx/type :tree-item
+                                    :value "Apte"}
+                                   {:fx/type :tree-item
+                                    :value "MW"}]}]}}))
+
+(defn component:top-row [& {:keys [zoom row column input]}]
+  {:fx/type :v-box
+   :grid-pane/column 0
+   :grid-pane/row 0
+   :children [{:fx/type :tool-bar
+               :items [(component:word-input-combo (-> input :current :original) (:history input))
+                       {:fx/type :separator}
+                       {:fx/type :label
+                        :text "Zoom"}
+                       (component:zoom-combo zoom)]}]})
+
+(defn component:middle-row:translation [& {:keys [input? dicts input row column]}]
+  (timbre/debug ::component:middle-row:translation)
+  {:fx/type :grid-pane
+   :grid-pane/column column
+   :grid-pane/row row
+   :column-constraints [{:fx/type :column-constraints
+                         :percent-width 30} ;; word list
+                        {:fx/type :column-constraints
+                         :percent-width 70}] ;; translation
+   :row-constraints [{:fx/type :row-constraints
+                      :percent-height 100}]
+   :children [; word list
+              (component:word-list (:history input)
+                                   :column 0
+                                   :row 0)
+              ;; translation
+              {:fx/type :tab-pane
+               :grid-pane/row 0
+               :grid-pane/column 1
+               :tabs [{:fx/type :tab
+                       :text "Translation"
+                       :closable false
+                       :content (component:translation (if input? (str (-> input :current :original) "<br><br>" sample)
+                                                           "Type and press enter to start searching..."))}
+                      {:fx/type :tab
+                       :text "Dictionaries"
+                       :closable false
+                       :content (components:dictionaries-checkboxes dicts)}]}]})
+
 
 (defn stage:main [{:keys [fx/context]}]
-  (timbre/debug ::stage:main :start)
+  (timbre/debug ::stage:main)
   (let [title (fx/sub-ctx context sub:title) ;; sample use of sub function
+        current-view (fx/sub-val context :current-view)
         input (fx/sub-val context :input)
         settings (fx/sub-val context :settings)
         input? (not= "" (-> input :current :original))
         zoom (:zoom settings)
+        dicts (fx/sub-val context :dictionaries)
         status (fx/sub-val context :status)
         style (css/register ::style {".root" {:-fx-font-size (str (* zoom 0.01) "em")}})]
     (timbre/debug ::stage:main :loaded-subs {:settings settings})
@@ -204,39 +269,12 @@
                                       {:fx/type :row-constraints
                                        :percent-height 5}] ;; status bar row
                     :children [;; Top row: toolbar (input, dictionaries, zoom)
-                               {:fx/type :v-box
-                                :grid-pane/column 0
-                                :grid-pane/row 0
-                                :children [{:fx/type :tool-bar
-                                            :items [(component:word-input-combo (-> input :current :original) (:history input))
-                                                    {:fx/type :separator}
-                                                    {:fx/type :label
-                                                     :text "Dictionaries"}
-                                                    (component:dictionaries-combo :row 0 :column 1)
-                                                    {:fx/type :separator}
-                                                    {:fx/type :label
-                                                     :text "Zoom"}
-                                                    (component:zoom-combo zoom)]}]}
+                               (component:top-row :column 0 :row 0 :zoom zoom :input input)
 
                                ;; Middle row: word selector and translation
-                               {:fx/type :grid-pane
-                                :grid-pane/column 0
-                                :grid-pane/row 1
-                                :column-constraints [{:fx/type :column-constraints
-                                                      :percent-width 30} ;; word list
-                                                     {:fx/type :column-constraints
-                                                      :percent-width 70}] ;; translation
-                                :row-constraints [{:fx/type :row-constraints
-                                                   :percent-height 100}]
-                                :children [; word list
-                                           (component:word-list (:history input)
-                                                                :column 0
-                                                                :row 0)
-                                           ;; translation
-                                           (component:translation (if input?
-                                                                    (str (-> input :current :original) "<br><br>" sample)
-                                                                    "Type and press enter to start searching...") :column 1 :row 0)]}
+                               (component:middle-row:translation :row 1 :column 0 :input input :input? input? :dicts dicts)
 
+                               ;; Bottom row
                                (component:statusbar status :column 0 :row 2)]}}}))
 
 
