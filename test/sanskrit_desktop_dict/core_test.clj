cb84b5de8fbcbcb2306f0149136c1a447acc8e59
MB <mb@blaster.ai>
2022-07-09 15:20:18 +0100

    Remove test

--- test/sanskrit_desktop_dict/core_test.clj
+++ test/sanskrit_desktop_dict/core_test.clj
@@ -13,17 +13,3 @@
                      :translation ""
                      :dictionaries []}
            :history []}})
-
-(t/deftest new-search!
-  (let [new-context (core/new-search! test-context "foo")
-        input (:input new-context)
-        current (:current input)
-        history (:history input)]
-    ;; Check that only :input has changed
-    (t/is (= (dissoc test-context :input) (dissoc new-context :input))
-          "new-search! has changed context outside of :input")
-    (t/is (= current {:original "foo"
-                      :translation ""
-                      :transliteration ""
-                      :dictionaries []}))
-    (t/is (= history ["foo"]))))
